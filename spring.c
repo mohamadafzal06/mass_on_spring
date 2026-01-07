5a364ead46c4ea4f90b12fad7ed69ce985704e2f
Mohammad Afzal <mohamadafzal06@gmail.com>
2026-01-07 17:54:36 +0330

    feat: implement spring extension

--- spring.c
+++ spring.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <math.h>
 
 #define FPS 120
 
@@ -8,8 +9,43 @@
 #define THICK 5
 #define MASS_WIDTH 100
 
-#define FLOOR_Y HEIGHT*0.6
+#define FLOOR_Y (HEIGHT*0.6)
 
+#define NUM_SPRING_ELEMENTS 16
+#define SPRING_ELEMENT_LENGTH 40
+
+
+float x_mass;
+
+typedef struct SpringElement {
+    Vector2 start, end;
+} SpringElement;
+
+SpringElement spring_elements[SPRING_ELEMENT_LENGTH];
+
+void updateSpring() {
+}
+
+void drawSpring() {
+    float x_spring_delta = x_mass / NUM_SPRING_ELEMENTS;
+    float y_end = (float) sqrt(pow(SPRING_ELEMENT_LENGTH, 2) - pow(x_spring_delta, 2));
+
+    for (int i = 0; i < NUM_SPRING_ELEMENTS; i += 2) {
+        Vector2 start = {
+            x_spring_delta * (float) i, FLOOR_Y - (float) MASS_WIDTH / 2 + (float) SPRING_ELEMENT_LENGTH / 2
+        };
+        Vector2 end = {start.x + x_spring_delta, start.y - y_end};
+        spring_elements[i] = (SpringElement){start, end};
+        DrawLineEx(start, end, THICK, WHITE);
+    }
+
+    for (int i = 1; i < NUM_SPRING_ELEMENTS; i += 2) {
+        Vector2 start = spring_elements[i - 1].end;
+        Vector2 end = {start.x + x_spring_delta, start.y + y_end};
+        spring_elements[i - 1] = (SpringElement){start, end};
+        DrawLineEx(start, end, THICK, WHITE);
+    }
+}
 
 
 void drawFloor() {
@@ -18,18 +54,17 @@ void drawFloor() {
     DrawLineEx(start, end,THICK,ORANGE);
 }
 
-void drawMass(float x) {
-    Rectangle rect = {x, FLOOR_Y - MASS_WIDTH - THICK / 2, MASS_WIDTH, MASS_WIDTH};
+void drawMass() {
+    Rectangle rect = {x_mass, FLOOR_Y - MASS_WIDTH - THICK / 2, MASS_WIDTH, MASS_WIDTH};
     DrawRectangleRec(rect, YELLOW);
 }
 
 int main(void) {
     InitWindow(WIDTH, HEIGHT, "Spring on Mass Simulation");
 
-    float x = 0;
     SetTargetFPS(FPS);
     float dt;
-    float v = 20;
+    float v = 40;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -37,8 +72,9 @@ int main(void) {
         DrawFPS(10, 10);
         dt = GetFrameTime();
         drawFloor();
-        x += v * dt;
-        drawMass(x);
+        x_mass += v * dt;
+        drawMass(x_mass);
+        drawSpring();
         DrawText("Spring on Mass Simulating", WIDTH / 10, 100, 30, RED);
         EndDrawing();
     }
